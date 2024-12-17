0efbccd03ad60fa10185043b02ccb0f5392d89de
Cville Paul <90533299+CvillePaul@users.noreply.github.com>
2024-12-16 21:22:32 -0500

    feat: add code to pivot readme rows by LST feat: add function to assign RV Standards to science targets

--- astropaul/lbt/lbt.py
+++ astropaul/lbt/lbt.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+import astropaul.observing as obs
 from astropaul.targetlistcreator import TargetList
 from .exposure_time import pepsi_exptime
 
@@ -38,15 +39,40 @@ def add_pepsi_params(
     return answer
 
 
-def make_lbt_readme_table(target_list: pd.DataFrame) -> pd.DataFrame:
+def assign_rv_standards(tl: TargetList, science_types: list[str], rv_standard_type: str = "RV Standard", **kwargs) -> pd.DataFrame:
+    answer = tl.copy()
+    rv_standards = answer.target_list[answer.target_list["Target Type"] == rv_standard_type]
+    science_targets = answer.target_list[answer.target_list["Target Type"].isin(science_types)]
+
+    def find_standard(value: float, standards: pd.DataFrame) -> int:
+        closest_idx = (standards['Teff'] - value).abs().idxmin()
+        if closest_idx == closest_idx:
+            return standards.iloc[closest_idx]["Target Name"]
+        else:
+            return ""
+
+    science_targets["RV Standard"] = science_targets["Teff"].apply(find_standard, standards=rv_standards)
+    answer.target_list = answer.target_list.merge(science_targets[["Teff", "RV Standard"]], on="Teff", how="left")
+    answer.target_list["RV Standard"].fillna("", inplace=True)
+    return answer
+
+
+def make_lbt_readme_table(target_list: pd.DataFrame, beg_lst: float = 0) -> pd.DataFrame:
     targets = target_list.copy().sort_values("ra")
-    # TODO: put code to pivot rows to a given lst
+    # "pivot" the rows of the table so it starts with the first ra > beg_lst
+    i = 0
+    for _, row in targets.iterrows():
+        if row["ra"] > beg_lst:
+            break
+        i += 1
+    targets = pd.concat([targets.iloc[i:], targets.iloc[:i]], ignore_index=True)
+    # build up the output table
     readme = pd.DataFrame()
     readme["Target Name"] = targets["Target Name"]
     readme["RA"] = targets["RA"]
     readme["Dec"] = targets["Dec"]
     readme["Vmag"] = targets["Vmag"]
-    readme["Teff"] = targets["Teff"]
+    readme["Teff"] = [f"{val:.0f}" for val in targets["Teff"]]
     readme["Fiber"] = targets["PEPSI fiber"]
     readme["BLUE Cross Disperser"] = targets["PEPSI cd_blue"]
     readme["BLUE CD NExp"] = targets["PEPSI cd_blue_num_exp"]
@@ -61,8 +87,8 @@ def make_lbt_readme_table(target_list: pd.DataFrame) -> pd.DataFrame:
     return readme
 
 
-def write_lbt_readme_file(file_base: str, targets: pd.DataFrame) -> str:
-    readme = make_lbt_readme_table(targets)
+def write_lbt_readme_file(file_base: str, targets: pd.DataFrame, session: obs.ObservingSession) -> str:
+    readme = make_lbt_readme_table(targets, session.starting_lst - 60)
     # save target list as csv
     readme.to_csv(
         file_base + ".csv",
