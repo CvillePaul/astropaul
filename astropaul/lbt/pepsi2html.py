0c5c9bb04c28b4c86e12913239a55082e7a1fb6e
Cville Paul <90533299+CvillePaul@users.noreply.github.com>
2025-04-08 11:19:15 -0400

    feat: change program so it takes a list of directories, creating an html file for each

--- astropaul/lbt/pepsi2html.py
+++ astropaul/lbt/pepsi2html.py
@@ -9,53 +9,52 @@ from bokeh.plotting import figure, output_file, save
 from bokeh.models.tools import BoxZoomTool, PanTool, ResetTool
 import numpy as np
 
-def pepsi2html(file_patterns: list[str], outfile: str, verbose: bool=False) -> None:
-    output_file(outfile)
-    plots = []
-    for file_pattern in file_patterns:
-        for path in glob(file_pattern):
-            filename = os.path.basename(path)
-            print(f"{filename=}")
-            hdul = fits.open(path)
-            header = hdul[0].header
-            target = header["OBJECT"]
-            obstime = f"{header["DATE-OBS"]} {header["TIME-OBS"]}"
-            telescope = header["TELESCOP"]
-            arm = header["ARM"]
-            fiber = header["FIBER"]
-            cd = header["CROSDIS"]
-            exposure = header["EXPTIME"]
-            if not type(exposure) is float:
-                dt = datetime.strptime(exposure, "%H:%M:%S")
-                exposure = timedelta(hours=dt.hour, minutes=dt.minute, seconds=dt.second).total_seconds()
-            if (snr := header.get("SNR")) is None:
-                snr = 0
+spectrum_extensions = [".nor", ".bwl"]
 
-            data = hdul[1].data
-            title = (
-                f"{target:<23s}: {arm:>4s} arm, CD{cd:<14s}, {exposure:>4.0f}sec, SNR {snr:5d}, {obstime[:19]}  File: {filename:<30s}"
-            )
-            p = figure(
-                title=title,
-                sizing_mode="stretch_width",
-                tools=[BoxZoomTool(), PanTool(), ResetTool()],
-            )
-            p.line(data["Arg"], np.clip(data["Fun"], a_min=None, a_max=1.3), line_width=0.2)
-            p.xaxis.ticker.num_minor_ticks = 25
-            # p.add_layout(Label(x=np.min(data["Arg"]), y=1.2, x_units="data", y_units="data", text=f"Quality={quality}"))
+def pepsi2html(spectrum_dirs: list[str], out_dir: str = ".", verbose: bool = False) -> None:
+    for spectrum_dir_pattern in spectrum_dirs:
+        fixed_dir_pattern = spectrum_dir_pattern.replace('"', '') # powershell does crappy things w/ trailing backslashes
+        for spectrum_dir in glob(fixed_dir_pattern):
+            outfile = os.path.join(out_dir, f"PEPSI Spectra {os.path.basename(spectrum_dir)}.html")
+            output_file(outfile)
+            plots = []
+            spectrum_files = sum(
+                [glob(os.path.join(spectrum_dir, f"pepsi*{extension}")) for extension in spectrum_extensions], []
+            ) # the sum with [] is a trick to flatten a list of lists
+            for spectrum_file in spectrum_files:
+                filename = os.path.basename(spectrum_file)
+                hdul = fits.open(spectrum_file)
+                header = hdul[0].header
+                target = header["OBJECT"]
+                obstime = f"{header["DATE-OBS"]} {header["TIME-OBS"]}"
+                arm = header["ARM"]
+                cd = header["CROSDIS"]
+                exposure = header["EXPTIME"]
+                if not type(exposure) is float:
+                    dt = datetime.strptime(exposure, "%H:%M:%S")
+                    exposure = timedelta(hours=dt.hour, minutes=dt.minute, seconds=dt.second).total_seconds()
+                if (snr := header.get("SNR")) is None:
+                    snr = 0
 
-            # output_file(filename=f"{target}_{arm}.html", title=p.title)
-            # f = f"{target}_{arm[:1]}_{obstime[:13]}h{obstime[14:16]}m{obstime[17:19]}s.html"
-            # output_file(filename=f, title=p.title)
-            plots.append(p)
-            # save(p)
+                data = hdul[1].data
+                title = (
+                    f"{target:<23s}: {arm:>4s} arm, CD{cd:<14s}, {exposure:>4.0f}sec, SNR {snr:5d}, {obstime[:19]}  File: {filename:<30s}"
+                )
+                p = figure(
+                    title=title,
+                    sizing_mode="stretch_width",
+                    tools=[BoxZoomTool(), PanTool(), ResetTool()],
+                )
+                p.line(data["Arg"], np.clip(data["Fun"], a_min=None, a_max=1.3), line_width=0.2)
+                p.xaxis.ticker.num_minor_ticks = 25
+                plots.append(p)
+                # if verbose:
+                #     print(title)
+
+            save(column(plots, sizing_mode="scale_width"))
+            # to convert to PDF, open html file in browser and print to file
             if verbose:
-                print(title)
-
-        save(column(plots, sizing_mode="scale_width"))
-        # to convert to PDF, open html file in browser and print to file
-        if verbose:
-            print(f"Output saved to {outfile}")
+                print(f"Output saved to {outfile}")
 
 
 if __name__ == '__main__':
