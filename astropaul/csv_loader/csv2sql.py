9a3ef4990478a03ac09488440ba9b251168d2cc4
Cville Paul <90533299+CvillePaul@users.noreply.github.com>
2025-04-30 21:14:53 -0400

    feat: add data transformations, to, eg, convert sexagesimal coordinates to decimal

--- astropaul/csv_loader/csv2sql.py
+++ astropaul/csv_loader/csv2sql.py
@@ -1,8 +1,8 @@
 import argparse
 import configparser
-from functools import partial
+import inspect
 from pathlib import Path
-import typing
+import sys
 
 from astropy.coordinates import Angle
 import astropy.units as u
@@ -10,154 +10,166 @@ import networkx as nx
 import pandas as pd
 import sqlalchemy as sa
 
-ColumnTransformer = typing.Callable[[str, pd.DataFrame, pd.DataFrame], pd.DataFrame]
+
+class DataTransformation:
+    def __init__(self, example_data: pd.DataFrame):
+        self.example_data = example_data
+
+    def get_sql_columns(self) -> list[tuple[str, object]]:
+        return [(column_name, sa.String) for column_name in self.example_data.columns]
+
+    def do_transformation(self, source: pd.DataFrame, dest: pd.DataFrame) -> pd.DataFrame:
+        for column_name in self.example_data.columns:
+            dest[column_name] = source[column_name]
+        return dest
 
 
-def DefaultColumnTransformer(column_name: str, source: pd.DataFrame, dest: pd.DataFrame) -> pd.DataFrame:
-    dest_column_name = db_style_to_string(column_name)
-    dest[dest_column_name] = source[column_name]
-    return dest
+class InferredTypeTransformation(DataTransformation):
+
+    def __init__(self, example_data: pd.DataFrame):
+        super().__init__(example_data)
+        self.column_types = {}
+        for column_name in self.example_data.columns:
+            column_type = InferredTypeTransformation._series_to_column_type(self.example_data[column_name])
+            self.column_types[column_name] = column_type
+
+    @staticmethod
+    def _series_to_column_type(series: pd.Series) -> str:
+        if pd.api.types.is_integer_dtype(series):
+            return sa.Integer
+        elif pd.api.types.is_float_dtype(series):
+            return sa.Float
+        else:
+            return sa.String  # Default to string for categorical/text data
+
+    def get_sql_columns(self):
+        return [(column_name, column_type) for column_name, column_type in self.column_types.items()]
+
+    def do_transformation(self, source, dest):
+        for column_name, expected_column_type in self.column_types.items():
+            source_column_type = InferredTypeTransformation._series_to_column_type(source[column_name])
+            if source_column_type != expected_column_type:
+                raise ValueError(f"Column {column_name} should be type {expected_column_type} but seeing {source_column_type}")
+        return super().do_transformation(source, dest)
 
 
-def SexagesimalOrDecimalDegreesColumnTransformer(
-    column_name: str, source: pd.DataFrame, dest: pd.DataFrame, hourangle: bool
-) -> pd.DataFrame:
-    if source.empty:
-        raise ValueError("Source DataFrame is empty")
-    dest_column_name = db_style_to_string(column_name)
-    sample_value = source.iloc[0][column_name]
-    if ":" in sample_value:
-        unit = u.hourangle if hourangle else u.deg
-        data = Angle(source[column_name], unit=unit)
-    else:
-        data = Angle(source[column_name], unit=u.deg)
-    dest[dest_column_name] = data.deg
-    dest[f"{dest_column_name} (HMS)"] = data.to_string(decimal=False, sep=":")
+class CoordinateDataTransformation(DataTransformation):
+
+    def __init__(self, example_data: pd.DataFrame):
+        super().__init__(example_data)
+        if len(self.example_data.columns) != 2:
+            raise ValueError("Must pass in exactly two columns (for RA & Dec) for this transformation")
+        self.ra_decimal, self.dec_decimal = self.example_data.columns
+        self.ra_sexagesimal = self.ra_decimal + "_hms"
+        self.dec_sexagesimal = self.dec_decimal + "_dms"
+
+    @staticmethod
+    def _get_angle_from_string(input: str, unit) -> Angle:
+        if ":" in input:
+            answer = Angle(input, unit=unit)
+        else:
+            answer = Angle(input, unit=u.deg)
+        return answer
+
+    def get_sql_columns(self):
+        return [
+            (self.ra_decimal, sa.Float),
+            (self.dec_decimal, sa.Float),
+            (self.ra_sexagesimal, sa.String),
+            (self.dec_sexagesimal, sa.String),
+        ]
+
+    def do_transformation(self, source, dest):
+        ra_angle = CoordinateDataTransformation._get_angle_from_string(source[self.ra_decimal], unit=u.hourangle)
+        dec_angle = CoordinateDataTransformation._get_angle_from_string(source[self.dec_decimal], unit=u.deg)
+        dest[self.ra_decimal] = ra_angle.deg
+        dest[self.dec_decimal] = dec_angle.deg
+        dest[self.ra_sexagesimal] = ra_angle.to_string(decimal=False, sep=":", pad=True)
+        dest[self.dec_sexagesimal] = dec_angle.to_string(decimal=False, sep=":", alwayssign=True, pad=True)
+        return dest
 
 
 class TableConfig:
-    def __init__(self):
-        self.table_name: str = "table"
-        self.has_id_column: bool = False
-        self.constraints: dict[str, tuple[str, str]] = dict()  # column name: (table name, column name) of foreign table
-        self.column_types: dict[str, object] = dict()  # manual column type overrides
-        self.column_transformations: dict[str, ColumnTransformer] = (
-            dict()
-        )  # how to change the csv contents into some other form
-
-    def validate_csv_schemas(self, data_files: list[tuple[Path, pd.DataFrame]]):
-        reference_file, reference_df = data_files[0]  # Use first file as reference
-        all_columns = set(reference_df.columns)
-        specified_columns = {}
-        for column, column_type in self.column_types.items():
-            if column not in all_columns:
-                raise ValueError(f"Column {column} specified in config file but not present in {reference_file}")
-            specified_columns[column] = column_type
-        inferred_columns = {
-            column: series_to_column_type(reference_df[column])
-            for column in reference_df.columns
-            if column not in specified_columns
-        }
-        self.column_types = {**inferred_columns, **specified_columns}
-        for column_name, column_type in self.column_types.items():
-            if column_name in self.column_transformations:
-                continue  # don't override manually specified column transformations
-            self.column_transformations[column_name] = DefaultColumnTransformer
-        for file_name, data_file in data_files[1:]:  # validate all remaining files against schema we just built
-            df_columns = set(data_file.columns)
-            if df_columns != all_columns:
-                raise ValueError(
-                    (
-                        f"Schema mismatch in {file_name} vs reference file {reference_file}. "
-                        "Expected columns: {all_columns}, Found: {df_columns}"
-                    )
-                )
-            for column, column_type in inferred_columns.items():
-                this_type = series_to_column_type(data_file[column])
-                if this_type != column_type:
-                    raise ValueError(
-                        (f"Data type mismatch in {file_name} for column {column}: " "{this_type} instead of {column_type}")
-                    )
-
-    @classmethod
-    def from_config_file(cls, config_file: Path) -> "TableConfig":
-        answer = TableConfig()
-        answer.table_name = config_file.name[:-4]
+    def __init__(self, config_file: Path, example_data: pd.DataFrame):
+        self.table_name = string_to_db_style(config_file.name[:-4])
+        self.has_id_column = False
+        self.transformations = []
+        self.constraints = {}  # key is dependent column, value is foreign (table, column)
+        specified_transformations = {}  # key is frozen set of input columns, value is transformation class
         if config_file.exists():
             config = configparser.ConfigParser()
             config.optionxform = str  # don't convert keys to lower case
             config.read(config_file)
-            answer.table_name = config.get("options", "table name", fallback=answer.table_name)
-            answer.has_id_column = config.getboolean("options", "create id column", fallback=False)
+            if specified_name := config.get("options", "table name", fallback=None):
+                self.table_name = string_to_db_style(specified_name)
+            self.has_id_column = config.getboolean("options", "create id column", fallback=False)
+            # process things that determine column order, transformations, type
+            unspecified_columns = set(example_data.columns)
             if config.has_section("columns"):
-                for column_name, column_type_str in config.items("columns"):
-                    if not (column_type := string_to_column_type(column_type_str)):
-                        raise ValueError(f"Unknown column type {column_type_str} in file {config_file}")
-                    answer.column_types[column_name] = column_type
-            if config.has_section("constraints"):
-                for column_name, foreign_column in config.items("constraints"):
-                    parts = foreign_column.split(".")
-                    if not parts or len(parts) != 2:
-                        raise ValueError(f"Bad constraint {foreign_column} for column {column_name} in {config_file}")
-                    foreign_table, foreign_column = parts
-                    answer.constraints[column_name] = (foreign_table, foreign_column)
+                for column_name, column_type in config.items("columns"):
+                    column_name = string_to_db_style(column_name)
+                    if not column_name in unspecified_columns:
+                        raise ValueError(f"Column type specified for unknown column {column_name} in {config_file}")
+                    if column_type != "str":
+                        raise ValueError(f"Unknown column type {column_type} for column {column_name} in file {config_file}")
+                    specified_transformations[frozenset([column_name])] = DataTransformation
+                    unspecified_columns.remove(column_name)
             if config.has_section("transformations"):
-                for column_name, transformer_name in config.items("transformations"):
-                    match transformer_name.lower():
-                        case "hms_or_decimal_degrees":
-                            transformer = partial(SexagesimalOrDecimalDegreesColumnTransformer, hourangle=True)
-                        case "dms_or_decimal_degrees":
-                            transformer = partial(SexagesimalOrDecimalDegreesColumnTransformer, hourangle=False)
-                        case _:
-                            raise ValueError(
-                                f"Unknown column transformation {transformer_name} for column {column_name} of {answer.table_name}"
-                            )
-                    answer.column_transformations[column_name] = transformer
-        return answer
+                current_module = sys.modules[__name__]
+                for transformation_name, column_list in config.items("transformations"):
+                    transformation_class = getattr(current_module, transformation_name, None)
+                    if not (
+                        transformation_class
+                        and inspect.isclass(transformation_class)
+                        and transformation_class.__module__ == __name__
+                    ):
+                        raise ValueError(f"Unknown column transformer {transformation_name}")
+                    transformation_columns = frozenset([column_name.lower().strip() for column_name in column_list.split(",")])
+                    specified_transformations[transformation_columns] = transformation_class
+                    unspecified_columns -= transformation_columns
+            # now handle config items dealing with data relations
+            if config.has_section("constraints"):
+                for dependent_column, foreign_target in config.items("constraints"):
+                    parts = foreign_target.split(".")
+                    if not parts or len(parts) != 2:
+                        raise ValueError(f"Bad constraint {foreign_target} for column {dependent_column} in {config_file}")
+                    column_name = string_to_db_style(dependent_column)
+                    if not column_name in example_data.columns:
+                        raise ValueError(f"Constraint specified for unknown column {dependent_column} in {config_file}")
+                    foreign_table = string_to_db_style(parts[0])
+                    foreign_column = string_to_db_style(parts[1])
+                    self.constraints[column_name] = (foreign_table, foreign_column)
+        processed_columns = set()
+        for column_name in example_data.columns:
+            if column_name in processed_columns:
+                continue  # this column is already handled by an existing transformation
+            column_was_specified = False
+            for affected_columns, transformation_class in specified_transformations.items():
+                if column_name in affected_columns:
+                    transformation = transformation_class(example_data[affected_columns])
+                    column_was_specified = True
+                    processed_columns.update(affected_columns)
+            if not column_was_specified:
+                transformation = InferredTypeTransformation(example_data[[column_name]])
+                processed_columns.add(column_name)
+            self.transformations.append(transformation)
+        pass
 
 
 def string_to_db_style(name: str) -> str:
     """Converts a directory or CSV column name to its database equivalent"""
     return name.lower().replace(" ", "_")
 
-def db_style_to_string(name: str) -> str:
-    """Converts a name in database style back to a more pretty, human form"""
-    answer = name.replace("_", " ")
-    answer = answer.title()
-    for string in ["Jd", "Utc", "Id", "Dssi", "Ra", "Hms", "Dms"]:
-        answer = answer.replace(string, string.upper())
-    return answer
 
-def series_to_column_type(series: pd.Series) -> str:
-    if pd.api.types.is_integer_dtype(series):
-        return sa.Integer
-    elif pd.api.types.is_float_dtype(series):
-        return sa.Float
-    else:
-        return sa.String  # Default to string for categorical/text data
-
-
-def string_to_column_type(column_type_str: str) -> object:
-    match column_type_str.lower():
-        case "str":
-            column_type = sa.String
-        case "int":
-            column_type = sa.Integer
-        case "float":
-            column_type = sa.Float
-        case _:
-            column_type = None
-    return column_type
-
-
-def create_table(metadata, table_config: TableConfig):
+def create_table(metadata, table_config: TableConfig) -> int:
     table_columns = []
     if table_config.has_id_column:
         table_columns.append(sa.Column("id", sa.Integer, primary_key=True, autoincrement=True))
-    for column_name, column_type in table_config.column_types.items():
-        table_columns.append(sa.Column(string_to_db_style(column_name), column_type))
-    table = sa.Table(string_to_db_style(table_config.table_name), metadata, *table_columns)
-    return table
+    for transformation in table_config.transformations:
+        for sql_name, sql_type in transformation.get_sql_columns():
+            table_columns.append(sa.Column(sql_name, sql_type))
+    sa.Table(table_config.table_name, metadata, *table_columns)
+    return len(table_columns)
 
 
 def determine_table_order(table_configs: dict[str, TableConfig]) -> list[str]:
@@ -182,24 +194,27 @@ def insert_csv_data(
     # retrieve values of foreign key constraints for validation before inserting data
     validation_values = {}
     for child_column, (source_table_name, source_column) in table_config.constraints.items():
-        source_table = metadata.tables[string_to_db_style(source_table_name)]
+        source_table = metadata.tables[source_table_name]
         with engine.connect() as conn:
-            result = conn.execute(sa.select(source_table.c[string_to_db_style(source_column)]))
+            result = conn.execute(sa.select(source_table.c[source_column]))
             validation_values[child_column] = set([row[0] for row in result])
     # validate and insert rows from each data file
     num_rows = 0
-    for file_name, data_file in data_files:
+    for file_name, df_from_csv in data_files:
+        # verify constrained columns have allowed values
         for child_column in table_config.constraints.keys():
             valid_column_values = validation_values[child_column]
-            for value in data_file[child_column].unique():
+            for value in df_from_csv[child_column].unique():
                 if not value in valid_column_values:
                     raise ValueError(
                         f"In file {file_name} for table {table_config.table_name}, value {value} not found in {source_table_name}.{source_column}"
                     )
-        db_frame = data_file.copy()
-        db_frame.columns = [string_to_db_style(column) for column in db_frame.columns]
-        db_frame.to_sql(string_to_db_style(table_config.table_name), engine, if_exists="append", index=False)
-        num_rows += len(data_file)
+        # now build the DataFrame we'll convert to sql
+        df_for_sql = pd.DataFrame()
+        for transformation in table_config.transformations:
+            df_for_sql = transformation.do_transformation(df_from_csv, df_for_sql)
+        df_for_sql.to_sql(table_config.table_name, engine, if_exists="append", index=False)
+        num_rows += len(df_from_csv)
     return num_rows
 
 
@@ -210,7 +225,9 @@ def get_data_files(data_dir: Path) -> list[tuple[str, pd.DataFrame]]:
             continue  # only process CSV files
         if file.name[0] == "#":
             continue  # skip files with "commented out" names
-        data_files.append((file, pd.read_csv(file)))
+        df = pd.read_csv(file)
+        df.columns = [string_to_db_style(column_name) for column_name in df.columns]
+        data_files.append((file, df))
     return data_files
 
 
@@ -231,20 +248,18 @@ def csv2sql(base_dir: str, outfile: str, verbose: bool = False):
             if not table_dir.is_dir():
                 continue  # Skip files, only process directories
             data_files = get_data_files(table_dir)
-            if not data_files:
+            if len(data_files) == 0:
                 if verbose:
-                    print(f"Skipping directory with no data files: {table_dir}")
+                    print(f"Skipping directory with no data files found: {table_dir}")
                 continue
-            data_files_by_table[table_name] = data_files
+            data_files_by_table[string_to_db_style(table_name)] = data_files
             config_file = table_dir / f"{table_dir.name}.ini"
-            table_config = TableConfig.from_config_file(config_file)
-            table_config.validate_csv_schemas(data_files)
+            table_config = TableConfig(config_file, data_files[0][1])
             table_configs[table_config.table_name] = table_config
-            create_table(metadata, table_config)
+            num_columns = create_table(metadata, table_config)
             if verbose:
-                print(f"Created table {table_name} with {len(table_config.column_types)} columns")
+                print(f"Created table {table_name} with {num_columns} columns, {len(table_config.constraints)} constraints")
         metadata.create_all(engine)
-
         # load data into the new tables
         table_order = determine_table_order(table_configs)
         for table_name in table_order:
