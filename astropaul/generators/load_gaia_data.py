32597ba10cb83d39b8599fb451c83f189ca05177
Cville Paul <90533299+CvillePaul@users.noreply.github.com>
2026-06-23 11:09:18 -0400

    feat: update query to get dr3 data from dr2 ids

--- astropaul/generators/load_gaia_data.py
+++ astropaul/generators/load_gaia_data.py
@@ -3,51 +3,54 @@ import os
 from pathlib import Path
 import sys
 
-from astropaul.database import database_connection
-from astropy.table import Table
-from astroquery.utils.tap.core import TapPlus
+from astroquery.gaia import Gaia
+from astropy.table import Column
 import pandas as pd
 
+from astropaul.database import database_connection, database_path
 
-def create_gaia_catalog_members(database:str = None, out_dir: str = ".", verbose: bool = False) -> None:
-    """Create Gaia DR3 associations from target names"""
-
-def load_gaia_data(database: str = None, out_dir: str = ".", verbose: bool = False) -> None:
+def load_gaia_data(out_dir: Path = None, verbose: bool = True) -> None:
     """Make a csv of Gaia DR3 data for all possible entries in the TESS database table."""
 
+    out_dir = out_dir or database_path().parent
 
-    # get table of all our targets that are mapped to a TESS TIC id
-    tic_query = "select target_name, catalog_id from catalog_members where catalog_name = 'TESS TICv8';"
-    with database_connection(database) as conn:
-        tic_targets = Table.from_pandas(pd.read_sql(tic_query, conn))
+    # get the gaia DR2 source IDs for all targets that have TESS data
+    dr2_query = """
+        select cast(gaia as INT) gaia_dr2, target_name
+        from tess_ticv8
+        where gaia is not null
+        ;
+    """
+    with database_connection() as conn:
+        dr2_to_target_name = pd.read_sql(dr2_query, conn, index_col="gaia_dr2").to_dict()["target_name"]
 
-    tic_targets = tic_targets[:1]
-
-    with TapPlus(url="https://mast.stsci.edu/api/v0/tap/tap") as mast_tap: 
-
-        upload_name = "target_list"
-        mast_tap.upload_table(upload_resource=upload_name, table=tic_targets)
-
-        # Build ADQL query
-        mast_query = f"""
-        SELECT targets.target_name, dr3.*
-        FROM tap_upload.{upload_name} targets
-        JOIN tic.tic as tess
-        ON tess.ticid = targets.catalog_id
-        JOIN gaia.dr3lite as dr3
-        ON tess.dr3_source_id = dr3.source_id
+    # retrieve dr3 data using the dr2 id list
+    gaia_query = f"""
+        select *
+        FROM gaiaedr3.gaia_source AS gaia
+        JOIN gaiaedr3.dr2_neighbourhood AS xmatch
+          ON gaia.source_id = xmatch.dr3_source_id
+        WHERE xmatch.dr2_source_id IN ({",".join(map(str, dr2_to_target_name.keys()))})
         """
-
-        job = mast_tap.launch_job(mast_query)
+    try:
+        job = Gaia.launch_job(gaia_query)
         results = job.get_results()
+    except Exception as e:
+        print(e)
 
-        mast_tap.delete_table(upload_name)
-
-    out_file = Path(out_dir) / "Gaia DR3.csv"
+    results.add_column(Column([dr2_to_target_name[dr2] for dr2 in results["dr2_source_id"]], name="Target Name"), index=0)
+    out_file = out_dir / "Gaia DR3" / "Gaia DR3.csv"
     results.write(out_file, overwrite=True)
+
+    out_file = out_dir / "Catalog Members" / "Gaia DR3.csv"
+    members = pd.DataFrame()
+    members["Target Name"] = dr2_to_target_name.values()
+    members["Catalog Name"] = "Gaia DR3"
+    members["Catalog ID"] = dr2_to_target_name.keys()
+    members.to_csv(out_file, index=False)
+
     if verbose:
         print(f"Wrote {len(results)} rows to {out_file}")
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
