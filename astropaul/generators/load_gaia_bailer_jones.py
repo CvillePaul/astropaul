cd41e78c6e1845210c43e5b4c41bc0d8058ed985
Cville Paul <90533299+CvillePaul@users.noreply.github.com>
2026-07-03 13:33:57 -0400

    feat: add upper & lower distance limits

--- astropaul/generators/load_gaia_bailer_jones.py
+++ astropaul/generators/load_gaia_bailer_jones.py
@@ -24,7 +24,7 @@ def load_gaia_bailer_jones(out_dir: Path = None, verbose: bool = True) -> None:
 
     # retrieve bailer-jones distance from the dr3 ids
     gaia_query = f"""
-        select source_id, r_med_geo
+        select source_id, r_med_geo, r_hi_geo, r_lo_geo
         FROM external.gaiaedr3_distance
         WHERE source_id IN ({",".join(map(str, dr3_to_target_name.keys()))})
         """
@@ -34,9 +34,12 @@ def load_gaia_bailer_jones(out_dir: Path = None, verbose: bool = True) -> None:
     except Exception as e:
         print(e)
 
-    # add target name as first column, remove source_id
+    # add target name as first column, remove source_id, rename distance column
     results.add_column(Column([dr3_to_target_name[str(dr3)] for dr3 in results["source_id"]], name="Target Name"), index=0)
     results.remove_column("source_id")
+    results.rename_column("r_med_geo", "Bailer Jones Distance")
+    results.rename_column("r_lo_geo", "Bailer Jones Lower Limit")
+    results.rename_column("r_hi_geo", "Bailer Jones Upper Limit")
 
     # write out distance data
     out_file = out_dir / "Gaia Bailer Jones" / "Gaia Bailer Jones.csv"
